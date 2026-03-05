991691efdee074a899a13650bcc601fc013a210b
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-05 15:07:31 -0300

    feat: 09 rotation with nice trick

--- technical-fundamentals/coding/problems/09_stringRotation.ts
+++ technical-fundamentals/coding/problems/09_stringRotation.ts
@@ -5,5 +5,13 @@ import { isSubstring } from "./__utils__/strings"
 // Assume you have a method isSubstring which checks if one word is a substring of another.
 // Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring.
 // [e.g., "waterbottle" is a rotation of 'erbottlewat")
+// Any rotation can partitioned in two:
+// S1 = x + y 
+// S2 = y + x
+// in the example was x = wat, y = erbottle => S1 = waterbottle S2 = erbottlewat
+// If if I concatenate S1 with itself is S1 + S1 = x + y + x + y = x + (y + x) + y = x + S2 + y
+// then s2 is substring of 2S1.
 
-export default function stringRotation(s1: string, s2: string): boolean {}
+export default function stringRotation(s1: string, s2: string): boolean {
+    return isSubstring(s1 + s1, s2)
+}
