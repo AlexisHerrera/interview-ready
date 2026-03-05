6b859c720d6ad656a9eb1bc28aa1cedfa0010d22
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-05 12:56:01 -0300

    feat: 08 zero matrix

--- technical-fundamentals/coding/problems/08_zeroMatrix.ts
+++ technical-fundamentals/coding/problems/08_zeroMatrix.ts
@@ -5,5 +5,34 @@
 type Matrix = number[][]
 
 export default function zeroMatrix (matrix: Matrix) {
+    const columns = new Set<number>();
+    const rows = new Set<number>();
 
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                rows.add(i)
+                columns.add(j)
+            }
+        }
+    }
+
+    function applyZerosRow(index: number){
+        for (let j = 0; j < matrix[index].length; j++) {
+            matrix[index][j] = 0
+        }
+    }
+
+    function applyZerosColumn(index: number){
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][index] = 0
+        }
+    }
+    
+    for (const col of columns) {
+        applyZerosColumn(col)
+    }
+    for (const row of rows) {
+        applyZerosRow(row)
+    }
 }
