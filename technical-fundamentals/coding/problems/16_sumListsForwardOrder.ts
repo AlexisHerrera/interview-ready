4c8b09868e154f0449e89cd7022d8ed59081e211
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-13 17:26:50 -0300

    feat: 16 - first (naive) solution

--- technical-fundamentals/coding/problems/16_sumListsForwardOrder.ts
+++ technical-fundamentals/coding/problems/16_sumListsForwardOrder.ts
@@ -16,4 +16,36 @@ export type Node<T> = {
 export default function sumListsForwardOrder(
   list1: Node<number> | undefined,
   list2: Node<number> | undefined,
-): Node<number> | undefined {}
+): Node<number> | undefined {
+  function getNumber(node: Node<number> | undefined): number | undefined {
+      if (node === undefined) {
+        return undefined
+      }
+      let numberStr = ''
+      let curr: Node<number> | undefined  = node
+      while (curr) {
+        numberStr = `${numberStr}${curr.value}`
+        curr = curr.next
+      }
+      return parseInt(numberStr)
+    }
+  function numberToList(num: number | undefined): Node<number> | undefined {
+    if (num === undefined || isNaN(num)) {
+      return undefined
+    }
+    const numStr = String(num)
+    let lastNode: Node<number> | undefined = undefined;
+    for (let i = 0; i < numStr.length; i++) {
+        const newNode: Node<number> = { value: parseInt(numStr[numStr.length - 1 - i]), next: lastNode}
+        lastNode = newNode
+    }
+    return lastNode
+  }
+  const num1 = getNumber(list1)
+  const num2 = getNumber(list2)
+  if (num1 === undefined && num2 === undefined) {
+    return undefined
+  }
+  const resultNumber = (getNumber(list1) ?? 0) + (getNumber(list2) ?? 0)
+  return numberToList(resultNumber)
+}
