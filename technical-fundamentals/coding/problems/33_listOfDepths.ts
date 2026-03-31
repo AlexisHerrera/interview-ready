e9dd9e9d351289c2e912febbfb8cc87a4462fae8
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-31 20:11:40 -0300

    feat: 33 - solves it in an (inefficient) way

--- technical-fundamentals/coding/problems/33_listOfDepths.ts
+++ technical-fundamentals/coding/problems/33_listOfDepths.ts
@@ -17,4 +17,33 @@ export type ListNode<T> = {
 
 export default function listOfDepths<T>(
   root: TreeNode<T> | null,
-): ListNode<T>[] {}
+): ListNode<T>[] {
+  if (root == null) {
+    return [];
+  }
+  let queue = [root];
+  const result: ListNode<T>[] = [];
+  while (queue.length > 0) {
+    const currLevel: TreeNode<T>[] = [];
+    const nextLevelNodes = [];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      if (!node) continue;
+      currLevel.push(node);
+      if (node.left) nextLevelNodes.push(node.left);
+      if (node.right) nextLevelNodes.push(node.right);
+    }
+    queue = [...nextLevelNodes];
+    if (currLevel.length == 0) continue;
+    const head: ListNode<T> = { value: currLevel[0].value };
+    let lastNode = head;
+    for (let i = 1; i < currLevel.length; i++) {
+      const currentNode = { value: currLevel[i].value };
+      lastNode.next = currentNode;
+      lastNode = currentNode;
+    }
+    result.push(head);
+  }
+
+  return result;
+}
