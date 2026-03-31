4341390756bf5714c6ec477befcc1fc4d03fd540
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-31 15:13:02 -0300

    feat: 30 - bfs and dfs for trees

--- technical-fundamentals/coding/problems/30_trees.ts
+++ technical-fundamentals/coding/problems/30_trees.ts
@@ -6,8 +6,45 @@ export type TreeNode<T> = {
   right?: TreeNode<T>;
 };
 
-export class Tree<T> {
-  bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+// 1 - 2 - 3
+// 1 - 4
+// 1 - 5 - 6 - 7
+// [3, 5, 4] visited: {1, 2}
 
-  dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+export class Tree<T> {
+  bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+    if (node === undefined) {
+      return;
+    }
+    const queue = [node];
+
+    while (queue.length > 0) {
+      const curNode = queue.shift();
+      visit(curNode as TreeNode<T>);
+      if (curNode?.left) {
+        queue.push(curNode.left);
+      }
+      if (curNode?.right) {
+        queue.push(curNode.right);
+      }
+    }
+  }
+
+  dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+    if (node === undefined) {
+      return;
+    }
+    const stack = [node];
+
+    while (stack.length > 0) {
+      const curNode = stack.pop();
+      visit(curNode as TreeNode<T>);
+      if (curNode?.right) {
+        stack.push(curNode.right);
+      }
+      if (curNode?.left) {
+        stack.push(curNode.left);
+      }
+    }
+  }
 }
