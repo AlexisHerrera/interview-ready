bf066b5b792b0d65acd30c0c1234c003a53bfc4f
Alexis Herrera <alexysaguilar@gmail.com>
2026-03-31 22:53:17 -0300

    feat: 34 - check balanced

--- technical-fundamentals/coding/problems/34_checkBalanced.ts
+++ technical-fundamentals/coding/problems/34_checkBalanced.ts
@@ -5,11 +5,38 @@
 // such that the heights of the two subtrees of any node never differ by more than one.
 
 export type TreeNode<T> = {
-    value: T;
-    left?: TreeNode<T>;
-    right?: TreeNode<T>;
+  value: T;
+  left?: TreeNode<T>;
+  right?: TreeNode<T>;
 };
 
-export default function checkBalanced<T>(tree?: TreeNode<T> | null): boolean {
+interface CallResult {
+  balanced: boolean;
+  length: number;
+}
 
+export default function checkBalanced<T>(tree?: TreeNode<T> | null): boolean {
+  function lengthTree(tree?: TreeNode<T> | null): CallResult {
+    if (tree == null) {
+      return { balanced: true, length: 0 };
+    }
+    const callLeft = lengthTree(tree.left);
+    if (!callLeft.balanced) {
+      return callLeft;
+    }
+    const callRight = lengthTree(tree.right);
+    if (!callRight.balanced) {
+      return callRight;
+    }
+
+    const lengthLeft = callLeft.length + 1;
+    const lengthRight = callRight.length + 1;
+    const returnValue = {
+      balanced: Math.abs(lengthLeft - lengthRight) <= 1,
+      length: Math.max(lengthLeft, lengthRight),
+    };
+
+    return returnValue;
+  }
+  return lengthTree(tree).balanced;
 }
