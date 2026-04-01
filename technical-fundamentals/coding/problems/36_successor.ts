3fd1c2b716304ca10d556c5e243e10df881119fb
Alexis Herrera <alexysaguilar@gmail.com>
2026-04-01 16:11:29 -0300

    feat: 36 successor

--- technical-fundamentals/coding/problems/36_successor.ts
+++ technical-fundamentals/coding/problems/36_successor.ts
@@ -5,12 +5,33 @@
 // You may assume that each node has a link to its parent.
 
 export type TreeNode<T> = {
-    value: T;
-    left?: TreeNode<T>;
-    right?: TreeNode<T>;
-    parent?: TreeNode<T>; // Link to parent node
+  value: T;
+  left?: TreeNode<T>;
+  right?: TreeNode<T>;
+  parent?: TreeNode<T>; // Link to parent node
 };
 
-export default function successor<T>(node: TreeNode<T>): TreeNode<T> | undefined {
-    
+export default function successor<T>(
+  node: TreeNode<T>,
+): TreeNode<T> | undefined {
+  if (node == null) {
+    return undefined;
+  }
+  if (node.right == null) {
+    let prev = node;
+    let parent = node.parent;
+    while (parent?.right == prev) {
+      prev = parent;
+      parent = parent.parent;
+    }
+    return parent;
+  }
+
+  let lastNode = node.right;
+  let currNode = node.right.left;
+  while (currNode) {
+    lastNode = currNode;
+    currNode = currNode.left;
+  }
+  return lastNode;
 }
